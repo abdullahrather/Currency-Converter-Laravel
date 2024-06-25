817ab80f1d002e33ae2836e2042b75a9692c6d9f
abdullahrather <abdullahrather19@gmail.com>
2024-06-25 06:16:55 +0200

    switcher button added to switch between currencies

--- app/Http/Controllers/CurrencyConvertController.php
+++ app/Http/Controllers/CurrencyConvertController.php
@@ -8,42 +8,47 @@ use Illuminate\Support\Facades\Log;
 
 class CurrencyConvertController extends Controller
 {
-  protected $client;
+    protected $client;
 
-  public function __construct()
-  {
-    $this->client = new Client([
-      'base_uri' => 'https://v6.exchangerate-api.com/v6/5cc908376f994c93a74f0d43/',
-      'timeout'  => 2.0,
-      'verify'   => storage_path('app/cacert.pem'),
-    ]);
-  }
+    public function __construct()
+    {
+        // Initialize GuzzleHttp Client with base URI and options
+        $this->client = new Client([
+            'base_uri' => 'https://v6.exchangerate-api.com/v6/5cc908376f994c93a74f0d43/',
+            'timeout'  => 2.0,
+            'verify' => base_path('certificates/cacert.pem'),
+        ]);
+    }
 
-  public function index()
-  {
-    // Fetch currencies using ExchangeRateApi
-    $response = $this->client->request('GET', 'latest/USD');
-    $rates = json_decode($response->getBody()->getContents(), true)['conversion_rates'];
+    public function index()
+    {
+        // Fetch latest currency rates using ExchangeRateApi
+        $response = $this->client->request('GET', 'latest/USD');
+        $rates = json_decode($response->getBody()->getContents(), true)['conversion_rates'];
 
-    return view('index', compact('rates'));
-  }
+        // Pass currency rates to the view
+        return view('index', compact('rates'));
+    }
 
-  public function convert(Request $request)
-  {
-    $request->validate([
-      'amount' => 'numeric|min:0',
-      'from' => 'required',
-      'to' => 'required',
-    ]);
+    public function convert(Request $request)
+    {
+        // Validate user input
+        $request->validate([
+            'amount' => 'numeric|min:0',
+            'from' => 'required',
+            'to' => 'required',
+        ]);
 
-    // Fetch conversion using ExchangeRateApi
-    $response = $this->client->request('GET', 'pair/' . $request->from . '/' . $request->to . '/' . $request->amount);
-    $converted = json_decode($response->getBody()->getContents(), true)['conversion_result'];
+        // Fetch currency conversion using ExchangeRateApi
+        $response = $this->client->request('GET', 'pair/' . $request->from . '/' . $request->to . '/' . $request->amount);
+        $converted = json_decode($response->getBody()->getContents(), true)['conversion_result'];
 
-    $conversionMessage = "{$request->amount} {$request->from} is equal to {$converted} {$request->to}";
+        // Prepare conversion message
+        $conversionMessage = "{$request->amount} {$request->from} is equal to {$converted} {$request->to}";
 
-    return back()->with([
-      'conversion' => $conversionMessage
-    ])->withInput(); // Ensure the input data is flashed to the session
-  }
+        // Redirect back with conversion message and input data
+        return back()->with([
+            'conversion' => $conversionMessage
+        ])->withInput();
+    }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,19 +2,21 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Laravel - Curency converter</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('https://fonts.googleapis.com/css?family=Alegreya+Sans:300,400,700,900') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css') }}">
-    <!-- Importing custom CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/css.css') }}">
+  <meta charset="UTF-8">
+  <title>Laravel - Curency converter</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet"
+    href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css') }}">
+  <link href="{{ asset('https://fonts.googleapis.com/css?family=Alegreya+Sans:300,400,700,900') }}" rel="stylesheet">
+  <link rel="stylesheet" href="{{ asset('https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css') }}">
+  <!-- Importing custom CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/css.css') }}">
 </head>
 
 <body>
-    <!-- Section that will be replaced by the content of the child views -->
-    @yield('main-section')
+  <!-- Section that will be replaced by the content of the child views -->
+  @yield('main-section')
 </body>
 
 </html>
