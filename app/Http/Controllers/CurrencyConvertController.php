bf12ddd6e1fe0a907507db3fe5715a3fb51892bc
abdullahrather <abdullahrather19@gmail.com>
2024-06-25 20:48:13 +0200

    changes made to use services for sending requests to API

--- app/Http/Controllers/CurrencyConvertController.php
+++ app/Http/Controllers/CurrencyConvertController.php
@@ -2,34 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\CurrencyConversionService;
 use Illuminate\Http\Request;
-use GuzzleHttp\Client;
-use Illuminate\Support\Facades\Log;
 
 class CurrencyConvertController extends Controller
 {
-    protected $client;
+    protected $currencyService;
 
-    public function __construct()
+    // CurrencyConvertController constructor.
+    // @param CurrencyConversionService $currencyService Instance of CurrencyConversionService for currency conversion operations.
+    public function __construct(CurrencyConversionService $currencyService)
+
     {
-        // Initialize GuzzleHttp Client with base URI and options
-        $this->client = new Client([
-            'base_uri' => 'https://v6.exchangerate-api.com/v6/5cc908376f994c93a74f0d43/',
-            'timeout'  => 2.0,
-            'verify' => base_path('certificates/cacert.pem'),
-        ]);
+        $this->currencyService = $currencyService;
     }
 
+    // Display the currency conversion form.
+    // @return \Illuminate\Contracts\View\View for displaying the currency conversion form.
     public function index()
     {
-        // Fetch latest currency rates using ExchangeRateApi
-        $response = $this->client->request('GET', 'latest/USD');
-        $rates = json_decode($response->getBody()->getContents(), true)['conversion_rates'];
+        // Get the latest currency exchange rates from ExchangeRate-API
+        $rates = $this->currencyService->getLatestRates();
 
-        // Pass currency rates to the view
         return view('index', compact('rates'));
     }
 
+    // Convert currency based on user input.
+    // @param Request $request User input containing amount, from currency, and to currency.
     public function convert(Request $request)
     {
         // Validate user input
@@ -39,14 +38,12 @@ class CurrencyConvertController extends Controller
             'to' => 'required',
         ]);
 
-        // Fetch currency conversion using ExchangeRateApi
-        $response = $this->client->request('GET', 'pair/' . $request->from . '/' . $request->to . '/' . $request->amount);
-        $converted = json_decode($response->getBody()->getContents(), true)['conversion_result'];
+        // Convert currency using ExchangeRate-API service
+        $converted = $this->currencyService->convertCurrency($request->from, $request->to, $request->amount);
 
         // Prepare conversion message
         $conversionMessage = "{$request->amount} {$request->from} is equal to {$converted} {$request->to}";
 
-        // Redirect back with conversion message and input data
         return back()->with([
             'conversion' => $conversionMessage
         ])->withInput();
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use GuzzleHttp\Client;
+use App\Services\CurrencyConversionService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,21 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Register GuzzleHttp Client singleton for communicating with ExchangeRate-API
+        $this->app->singleton(Client::class, function ($app) {
+            return new Client([
+                // Best practice is to store API in .env file but in this task for the ease of project setup by the reviewer,
+                // I have hardcoded it in the base_uri
+                'base_uri' => 'https://v6.exchangerate-api.com/v6/5cc908376f994c93a74f0d43/',
+                'timeout'  => 2.0,
+                'verify' => base_path('certificates/cacert.pem'),
+            ]);
+        });
+
+        // Register CurrencyConversionService singleton with GuzzleHttp Client dependency
+        $this->app->singleton(CurrencyConversionService::class, function ($app) {
+            return new CurrencyConversionService($app->make(Client::class));
+        });
     }
 
     /**
